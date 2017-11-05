128345103736bb6a52ec83316b8c33ef88db3f7b
Sacheth Chandramouli <c.sacheth1@gmail.com>
2017-11-05 12:30:56 -0500

    base
    do npm install —save to install all dependencies

---
+++ front-end/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
