9a5d5ed5b57fde538bedaf0679bffa682b565f09
Samir Undavia <samir314159@gmail.com>
2017-11-13 15:08:10 -0500

    add db connection gem

--- API/api.rb
+++ API/api.rb
@@ -1,13 +1,12 @@
-# api.rb
-
 require 'sinatra'
+require 'sequel'
+
+DB = Sequel.connect(:adapter => 'mysql2', :user => 'soundify', :password=>'tomozpan', :host => 'soundify.ccj707h8lkgk.us-east-1.rds.amazonaws.com', :database => 'soundify')
 
 get '/test' do
-	response['Access-Control-Allow-Origin'] = '*'
-	'yo'
+	'test successful'
 end
 
-get '/playlist/#pl_id' do
-	response['Access-Control-Allow-Origin'] = '*'
-	
+get '/playlist/:pl_id' do
+    'get request for playlist with id ' + params['pl_id']
 end
