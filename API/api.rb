6dd494a2b2c070586e911bb821559d02a349726f
Samir Undavia <samir314159@gmail.com>
2017-11-19 20:13:16 -0500

    added some comments about how to send data with the api

--- API/api.rb
+++ API/api.rb
@@ -11,3 +11,11 @@ end
 get '/playlist/:pl_id' do
     'get request for playlist with id ' + params['pl_id']
 end
+
+post '/something_secure/' do # someome submits a form to /something_secure using post
+    # there's a field in a form where the name in the form tag is post_from_html_key
+    puts params['post_from_html_key'] # <input type="password" name="post_from_html_key" />
+    # puts prints stuff out to the serve console, nothing is returned to the user
+    # if you want to return something, do:
+    json {'keyyy': 'a test value'} # send a hash (dict) over json
+end
